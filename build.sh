db35046baa29b700c7b759a08e3169fcc7f6823e
Muhammed Shazin Lk <mohdshazinlk@gmail.com>
2026-03-18 11:43:44 +0530

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
+
+# create superuser automatically
+python manage.py shell << END
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.create_superuser('shazin', 'admin@gmail.com', 'slk123')
+END
