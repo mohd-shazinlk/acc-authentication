4a26247760ee62da2b07e7144f9e9288b7a8f459
Muhammed Shazin Lk <mohdshazinlk@gmail.com>
2026-03-18 11:56:56 +0530

    update

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput || true
 
 # create superuser automatically
 python manage.py shell << END
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-User.objects.create_superuser('shazin', 'admin@gmail.com', 'slk123')
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="shazin").exists():
+    User.objects.create_superuser("shazinlk", "admin@gmail.com", "slk123")
 END
